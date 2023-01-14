70c2c74f37cc77b3f24c08928cc57f9256761ea6
Greg Haskins <greg@manetu.com>
2023-01-14 12:03:58 -0500

    Update unit-test for new side-effect/now behavior
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- test/temporal/test/side_effect.clj
+++ test/temporal/test/side_effect.clj
@@ -7,7 +7,7 @@
             [temporal.workflow :refer [defworkflow]]
             [temporal.side-effect :as side-effect]
             [temporal.test.utils :as t])
-  (:import [java.time LocalTime]))
+  (:import [java.time Instant]))
 
 (use-fixtures :once t/wrap-service)
 
@@ -20,4 +20,4 @@
   (testing "Verifies that we can round-trip through start"
     (let [workflow (t/create-workflow side-effect-workflow)]
       (c/start workflow {})
-      (is (instance? LocalTime @(c/get-result workflow))))))
+      (is (instance? Instant @(c/get-result workflow))))))
