5308f07ba9edd300ebc8ed411f07e527f31f69f9
Greg Haskins <greg@manetu.com>
2023-03-22 16:52:16 -0400

    Add support for heartbeats
    Signed-off-by: Greg Haskins <greg@manetu.com>

---
+++ test/temporal/test/heartbeat.clj
@@ -0,0 +1,28 @@
+;; Copyright © 2023 Manetu, Inc.  All rights reserved
+;;
+(ns temporal.test.heartbeat
+  (:require [clojure.test :refer :all]
+            [temporal.client.core :as c]
+            [temporal.workflow :refer [defworkflow]]
+            [temporal.activity :refer [defactivity] :as a]
+            [temporal.test.utils :as t]))
+
+(use-fixtures :once t/wrap-service)
+
+(defactivity heartbeat-activity
+  [_ _]
+  (if-let [details (a/get-heartbeat-details)]
+    details
+    (do
+      (a/heartbeat :ok)
+      (throw (ex-info "heartbeat details not found" {})))))
+
+(defworkflow heartbeat-workflow
+  [_ _]
+  @(a/invoke heartbeat-activity {}))
+
+(deftest the-test
+  (testing "Verifies that heartbeats are handled properly"
+    (let [workflow (t/create-workflow heartbeat-workflow)]
+      (c/start workflow {})
+      (is (-> workflow c/get-result deref (= :ok))))))
