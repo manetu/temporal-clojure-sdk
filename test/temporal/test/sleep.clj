b8d93df279d4ac06e7bd850690f5a43652b57cba
nivekuil <mail@nivekuil.com>
2023-09-19 15:13:00 -0700

    add terminate test
    Signed-off-by: nivekuil <mail@nivekuil.com>

--- test/temporal/test/sleep.clj
+++ test/temporal/test/sleep.clj
@@ -5,7 +5,8 @@
             [taoensso.timbre :as log]
             [temporal.client.core :as c]
             [temporal.workflow :refer [defworkflow] :as w]
-            [temporal.test.utils :as t])
+            [temporal.test.utils :as t]
+            [promesa.core :as p])
   (:import [java.time Duration]))
 
 (use-fixtures :once t/wrap-service)
@@ -16,8 +17,10 @@
   (w/sleep (Duration/ofSeconds 1))
   :ok)
 
+(def workflow-id "sleep-workflow")
 (defn create []
-  (let [wf (c/create-workflow (t/get-client) sleep-workflow {:task-queue t/task-queue})]
+  (let [wf (c/create-workflow (t/get-client) sleep-workflow {:task-queue t/task-queue
+                                                             :workflow-id workflow-id})]
     (c/start wf nil)
     wf))
 
@@ -25,3 +28,12 @@
   (testing "Verifies that signals may timeout properly"
     (let [wf (create)]
       (is (= @(c/get-result wf) :ok)))))
+
+(deftest terminate-sleep
+  (testing "Verifies that sleeping workflows can be terminated"
+    (let [wf (create)]
+      (c/terminate (c/create-workflow (t/get-client) workflow-id) "reason" {})
+      (is (= :error
+             @(-> (c/get-result wf)
+                  (p/catch io.temporal.client.WorkflowFailedException
+                           (fn [_] :error))))))))
