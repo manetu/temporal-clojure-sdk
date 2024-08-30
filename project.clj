8231a5ac70b2482f6e012bb72af07998dcaada61
Greg Haskins <greg@manetu.com>
2024-08-30 16:32:08 -0400

    Release v1.0.3
    Changes since v1.0.2
    --------------------
    70cf629 Update deps
    e1a2dc6 Fix doc/clients.md schedule example
    0256b25 Add activity-id retry test
    f29d440 Add clj-kondo configuration
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.manetu/temporal-sdk "1.0.3-SNAPSHOT"
+(defproject io.github.manetu/temporal-sdk "1.0.3"
   :description "A Temporal SDK for Clojure"
   :url "https://github.com/manetu/temporal-clojure-sdk"
   :license {:name "Apache License 2.0"
