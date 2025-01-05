e124cea4ec6649e08f069d781b33030ce221dc38
Greg Haskins <greg@manetu.com>
2025-01-05 15:02:43 -0500

    Release v1.2.0
    Changes since v1.1.0
    --------------------
    1576bf4 Update to temporal-sdk 1.27.0 and add support for vthreads
    436b82c Use jdk23 image instead of cimg/clojure, since the JDK is lagging
    d669501 Fix misplaced default in :workflow-task-timeout documentation
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.manetu/temporal-sdk "1.1.1-SNAPSHOT"
+(defproject io.github.manetu/temporal-sdk "1.2.0"
   :description "A Temporal SDK for Clojure"
   :url "https://github.com/manetu/temporal-clojure-sdk"
   :license {:name "Apache License 2.0"
