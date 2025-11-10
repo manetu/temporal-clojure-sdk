d9fd09c260268eb505f67ac701fa0e302bd307ba
Greg Haskins <greg@manetu.com>
2025-11-10 13:07:30 -0500

    Prepare for v1.5.1 development
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.manetu/temporal-sdk "1.5.0"
+(defproject io.github.manetu/temporal-sdk "1.5.1-SNAPSHOT"
   :description "A Temporal SDK for Clojure"
   :url "https://github.com/manetu/temporal-clojure-sdk"
   :license {:name "Apache License 2.0"
